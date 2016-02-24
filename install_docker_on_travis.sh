f3e65a7bab62397840913df58c6d46bc850d39da
Steven Pollack <steven@gnobel.com>
2016-02-24 18:41:43 +0100

    changed cat -> echo in install script

--- install_docker_on_travis.sh
+++ install_docker_on_travis.sh
@@ -7,7 +7,7 @@ sudo apt-get install -y \
 
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
-sudo cat > /etc/apt/sources.list.d/docker.list <<APPEND
+sudo echo > /etc/apt/sources.list.d/docker.list << APPEND
 deb https://apt.dockerproject.org/repo ubuntu-trusty main
 APPEND
  
