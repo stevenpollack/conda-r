3663652256f43209df7d31327de5174060f54f78
Steven Pollack <steven@gnobel.com>
2016-02-24 19:13:06 +0100

    changed docker.list affection to check for existence

--- install_docker_on_travis.sh
+++ install_docker_on_travis.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-sudo apt-get update
+#sudo apt-get update
 sudo apt-get install -y \
   apt-transport-https ca-certificates \
   linux-image-extra-$(uname -r)
 
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
-sudo echo > /etc/apt/sources.list.d/docker.list << APPEND
+DOCKER_LIST=/etc/apt/sources.list.d/docker.list
+if [ ! -f $DOCKER_LIST ]; then
+  sudo touch $DOCKER_LIST
+fi
+
+sudo cat > $DOCKER_LIST << APPEND
 deb https://apt.dockerproject.org/repo ubuntu-trusty main
 APPEND
  
