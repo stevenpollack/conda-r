3b42e75177406f4aad91ee81f99a4da9832f84b3
Steven Pollack <steven@gnobel.com>
2016-02-24 13:20:58 +0100

    shifted use of sudo to inside script
    and not in the .travis.yml

--- install_docker_on_travis.sh
+++ install_docker_on_travis.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-apt-get update
-apt-get install -y \
+sudo apt-get update
+sudo apt-get install -y \
   apt-transport-https ca-certificates \
   linux-image-extra-$(uname -r)
 
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
-cat > /etc/apt/sources.list.d/docker.list <<APPEND
+sudo cat > /etc/apt/sources.list.d/docker.list <<APPEND
 deb https://apt.dockerproject.org/repo ubuntu-trusty main
 APPEND
  
-apt-get update
-apt-get purge lxc-docker
-apt-get install -y docker-engine
+sudo apt-get update
+sudo apt-get purge lxc-docker
+sudo apt-get install -y docker-engine
